456318bd2df8024745c4ad447b1f24be175f08c7
SOUMYA NAYAK <devsoumyanayak@gmail.com>
2022-09-23 22:18:14 +0530

    Initial commit

---
+++ app/src/main/java/com/ltts/aladdin/views/activity/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.ltts.aladdin.views.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ltts.aladdin.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
